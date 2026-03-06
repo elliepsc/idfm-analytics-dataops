835271e243853414b2d517f1feb6ae2e418a39ed
ElliePsc <ellie.pascaud@gmail.com>
2026-03-06 16:48:47 +0100

    dbt tests changes

--- warehouse/dbt/tests/assert_positive_values.sql
+++
@@ -1,8 +0,0 @@
-{% test assert_positive_values(model, column_name) %}
-
-SELECT {{ column_name }}
-FROM {{ model }}
-WHERE {{ column_name }} IS NOT NULL
-  AND {{ column_name }} < 0
-
-{% endtest %}
--- warehouse/dbt/tests/assert_valid_date_range.sql
+++
@@ -1,8 +0,0 @@
-{% test assert_valid_date_range(model, column_name, min_date='2020-01-01') %}
-
-SELECT {{ column_name }}
-FROM {{ model }}
-WHERE {{ column_name }} IS NOT NULL
-  AND {{ column_name }} < CAST('{{ min_date }}' AS DATE)
-
-{% endtest %}
